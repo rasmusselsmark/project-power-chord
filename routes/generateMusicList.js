82a088f7adba66498d9bb15ceb7a5556e03e92f4
unknown <magn444c@HTKskoler.dk>
2021-10-15 08:53:30 +0200

    Added music uploading etc.

--- routes/generateMusicList.js
+++ routes/generateMusicList.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 router.get('/', function(req, res) {
-  let trackData = {
-    
-  }
+  fs.readdir('routes/Audio', function (err, files) {
+    if (err) {
+      console.log(err)
+      return
+    }
+
+    files.forEach(function (file) {
+      let tracks = []
+      let trackData = {
+        name: file,
+        path: 'routes/Audio/' + file
+      }
+
+      tracks.push(trackData)
+    });
+  })
 });
 
 module.exports = router;
